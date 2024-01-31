786742bc51af3f566630d7eb4f93706584735c9c
Vanderlei Oliveira <vanderlei.oliveira@oowlish.com>
2024-01-31 18:57:33 -0300

    initial code

---
+++ webpack.common.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: './src/index.tsx',
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "index_bundle.js"
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      hash: true,
+      filename: "index.html",  //target html
+      template: "./src/index.html" //source html
+    })
+  ]
+};
