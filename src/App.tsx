de07743c44381be8619ccec7cefa3a37018bc6bb
Vanderlei Oliveira <vanderlei327@gmail.com>
2024-02-02 11:06:52 -0300

    react router dom and recoil

--- src/App.tsx
+++ src/App.tsx
@@ -1,78 +1,36 @@
 
-import React, { useEffect, useState } from 'react';
-import fetchWeather from './utils/fetchWeather';
-import { cardData } from './types/card';
+import React from 'react';
+import { RecoilRoot } from "recoil";
+import { RouterProvider, createBrowserRouter, Navigate } from 'react-router-dom';
+
 import { GlobalStyle, MainStyle } from './styles/mainStyle';
-import Input from './components/Input/index';
-
-import styled from "styled-components";
-import Button from './components/Button/index';
-
-export const AddData = styled.div`
-  width: 100%;
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  text-align: center;
-  
-  h1{
-    margin-bottom: 50px;
-  }
-`;
-
-export const TwoColumns = styled.div`
-display: flex;
-justify-content: center;
-column-count: 2;
-`
-
+import AddForecasts from './pages/add-forecast';
+import Forecasts from './pages/forecasts';
 
 const App: React.FC = () => {
 
-  const [weather, setWeather] = useState<cardData | {}>({});
+  const router = createBrowserRouter([
+    {
+      path: "/forecasts",
+      element: <Forecasts />
+    },
+    {
+      path: "/add-forecasts",
+      element: <AddForecasts />
+    },
+    {
+      path: "*",
+      element: <Navigate to="/forecasts" /> //Fallback to go to forecasts in case
+      // a user try to navigate to any other page not listed here
+    }
+  ])
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const data = await fetchWeather("-14.875", "-40.875");
-        setWeather(data);
-      } catch (error) {
-        console.error('Error fetching weather data:', error);
-      }
-    };
-
-    fetchData();
-
-  }, []);
-
-
-  return <>
+  return <RecoilRoot>
     <GlobalStyle />
     <MainStyle>
-      <AddData>
-        <h1>Entre com os dados de latitude e longitude<br />
-          para adicionar um novo card</h1>
-        <TwoColumns>
-          <Input
-            type="text"
-            label="Latitude"
-            required
-          />
-
-          <Input
-            type="text"
-            label="Longitude"
-            required
-          />
-        </TwoColumns>
-        <Button>
-          Adicionar
-        </Button>
-      </AddData>
+      <RouterProvider router={router} />
     </MainStyle >
-  </>;
+  </RecoilRoot>;
 };
 
 export default App;
